2999df4d28b85253bd94b649c141552f41f5e6bd
quickmute <hyon24@gmail.com>
2020-03-13 08:27:19 -0500

    null resource

--- main.tf
+++ main.tf
@@ -2,6 +2,13 @@ locals{
   test = "hello world"
 }
 
+resource "null_resource" "exampleB" {
+  triggers = {
+      key   = "hello"
+      value = "world"
+  }
+}
+
 output "test"{
   value = local.test
 }

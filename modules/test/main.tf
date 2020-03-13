87ae05f2fb3a5385a2ba691b8917b5b61c7bfef3
quickmute <hyon24@gmail.com>
2020-03-13 10:12:25 -0500

    ATLAS_WORKSPACE_NAME

--- modules/test/main.tf
+++ modules/test/main.tf
@@ -1,7 +1,17 @@
 ## hello
 variable "ATLAS_CONFIGURATION_SLUG"{
+  default = ""
   type = string
 }
+
+variable "ATLAS_WORKSPACE_NAME"{
+  type = string
+}
+
 output "hello_from_module"{
   value = var.ATLAS_CONFIGURATION_SLUG
+}
+
+output "workspace"{
+  value = var.ATLAS_WORKSPACE_NAME
 }

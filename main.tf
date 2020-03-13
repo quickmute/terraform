3fdf3d7bf30501602d00c902ab1703632b143736
quickmute <hyon24@gmail.com>
2020-03-13 08:58:13 -0500

    clean it up

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
-locals{
-  test = "hello world"
-  version = "1.1"
-}
-
 variable "ATLAS_WORKSPACE_NAME"{
 }
 
-resource "null_resource" "exampleB" {
+locals{
+  version = "1.1"
+}
+
+resource "null_resource" "example" {
   triggers = {
       key   = "hello"
       value = formatdate("YYYY-MM-DD hh:mm:ssZZZZZ", timestamp())
@@ -15,10 +14,6 @@ resource "null_resource" "exampleB" {
 
 output "version"{
   value = local.version
-}
-
-output "test"{
-  value = local.test
 }
 
 output "workspace"{
